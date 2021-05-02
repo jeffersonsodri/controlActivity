4dce7776f519bd9da5507a12c3b0f17a2253f80d
Jefferson Souza Rodrigues <jeffersoncacula@gmail.com>
2021-05-02 00:14:20 -0400

    Adicionando o react projeto

---
+++ aplicacao/src/components/home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './home.css'
+
+    
+export default (props) => (
+    <div>
+        <h1>
+            Meu amor é linda demais gata gostosa s2
+        </h1>
+    </div>
+);
+
+
+
+    
+        
+
+
+    
