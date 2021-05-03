269a224087cdcb9ecccf7188e4fa7ff7c653aa73
Jefferson Souza Rodrigues <jeffersoncacula@gmail.com>
2021-05-02 23:40:26 -0400

    Adicionando botao para a página inicial

--- aplicacao/src/components/home.jsx
+++ aplicacao/src/components/home.jsx
@@ -7,6 +7,9 @@ export default (props) => (
         <h1>
             Meu amor é linda demais gata gostosa s2
         </h1>
+        <button>To Do</button>
+        <button>Doing</button>
+        <button>Done</button>
     </div>
 );
 
